3cf36bc80097b072650634574436179b3fe8ffc4
ribrciti <rbciti@msn.com>
2014-09-23 07:12:25 -0700

    Added validations and the display of validation errors in the new product form

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -35,7 +35,7 @@ class ProductsController < ApplicationController
 
 private
   def product_params
-    params.require(:product).permit(:title)
+    params.require(:product).permit(:title, :description, :price, :stock_quantity)
   end
 end
 
