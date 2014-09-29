2f4a6be16f21a4b574050557f4cf90872dbd596b
ribrciti <rbciti@msn.com>
2014-09-29 06:08:23 -0700

    Added a scaffold for Categories.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
 
+  resources :categories
+
   get    'products',          to: 'products#index', as: 'products'
   get    'products/new',      to: 'products#new',   as: 'new_product'
   get    'products/:id',      to: 'products#show',  as: 'product'
