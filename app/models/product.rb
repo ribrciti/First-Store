4a6f8c8bb1bec3947e10b8281c1770014cbf5780
ribrciti <rbciti@msn.com>
2014-09-30 11:53:14 -0700

    Added Categories to our Procucts table

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,6 @@
 class Product < ActiveRecord::Base
+	belongs_to :category
+	
 	validates :title, :price, :stock_quantity, presence: true
 	validates :price, numericality: { greater_than_or_equal_to: 0.01} 
 	validates :stock_quantity, numericality: { only_integer: true}
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140929130241) do
+ActiveRecord::Schema.define(version: 20140929201037) do
 
   create_table "categories", force: true do |t|
     t.string   "name"
@@ -26,6 +26,9 @@ ActiveRecord::Schema.define(version: 20140929130241) do
     t.integer  "stock_quantity"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "category_id"
   end
+
+  add_index "products", ["category_id"], name: "index_products_on_category_id"
 
 end
